a31c6220dc27e90d97e1e7e7dc86616ca3ff39ff
rennyatwork <rennyatwork@gmail.com>
2014-10-12 13:07:48 -0400

    plot 3 and 4
    plot 3 and 4 - params

--- plot3.R
+++ plot3.R
@@ -6,6 +6,6 @@ plot3 <- function(pBty="y", pCex=0.6)
   plot(x=dt$dateTime, y=dt$Sub_metering_1, type='l' ,ylab="Energy sub metering", xlab="")
   points(x=dt$dateTime, y=dt$Sub_metering_2, type='l', col='red')
   points(x=dt$dateTime, y=dt$Sub_metering_3, type='l', col='blue')
-  legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lwd=1, cex=0.6)
+  legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lwd=1, cex=pCex, bty=pBty)
   saveAndClose("plot3")
 }
--- plot4.R
+++ plot4.R
@@ -10,7 +10,7 @@ plot4 <- function()
   # second graph:
     plot(x=dt$dateTime, y=dt$Voltage, type='l' ,ylab="Voltage", xlab="datetime")
   # third graph: see plot 3
-    plot3("n", "0.58")
+    plot3("n", 0.58)
   # fourth graph
     plot(x=dt$dateTime, y=dt$Global_reactive_power, type='l' ,ylab="Global Active Power", xlab="")
   
